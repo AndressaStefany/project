69b44f1a795b99793620f35521609b053eae154c
AndressaStefany <astefanysoliveira@gmail.com>
2018-06-16 19:54:59 -0300

    serviço correlação

--- back/api/data/views.py
+++ back/api/data/views.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 from django.views.decorators.csrf import csrf_protect
-
-# Create your views here.
+import os # os.getcwd()
 
 def dataFrameToJson(dataFrame):
     dataFrame = dataFrame.to_json(orient='records')
@@ -12,15 +11,16 @@ def dataFrameToJson(dataFrame):
 
     return dataFrame
 
+@csrf_protect
 def teste(request):
     limit = int(request.GET.get('limit'))
-    dataFrame = pd.read_csv('turmas.csv').head(limit)
+    dataFrame = pd.read_csv('data_science/turmas-2010-2017-ok.csv').head(limit)
 
-    return JsonResponse(dataFrameToJson(dataFrame))
+    return JsonResponse(dataFrameToJson(dataFrame),safe=False)
 
 @csrf_protect
 def testeGrafico(request):
-    dataFrame = pd.read_csv('matricula20172.csv',sep=';')
+    dataFrame = pd.read_csv('data_science/matriculas_new/matricula-componente-20172.csv',sep=';')
     dataFrame = dataFrame.dropna()
     dataFrame = dataFrame[dataFrame['nota']>0]
     colunas = ['discente', 'nota']
@@ -28,27 +28,14 @@ def testeGrafico(request):
 
     return JsonResponse({'results':dataFrameToJson(dataFrame)})
 
-@csrf_protect
-# Retorna correlação entre duas disciplinas informadas
-def correlacao(request):
-    discA = request.GET.get('discA')
-    discB = request.GET.get('discB')
-
-    dataFrame = pd.read_csv('data_science/df_disciplinas2015New.csv')
-
+# Retorna (int) correlação entre duas disciplinas informadas
+def simpleCorrelacao(discA,discB):
+    dataFrame = pd.read_csv('data_science/turmas_new.csv')
     dataFrameA = dataFrame[dataFrame['nome'] == discA]
     dataFrameB = dataFrame[dataFrame['nome'] == discB]
 
-    # filtrando apenas alunos que pagaram ambas as disciplinas
-    # series_discentesA = dataFrameA.discente.unique()
-    # series_discentesB = dataFrameB.discente.unique()
-    #
-    # dataFrameA = dataFrameA[dataFrameA['discente'].isin(series_discentesA)]
-    # dataFrameB = dataFrameB[dataFrameB['discente'].isin(series_discentesB)]
-
     # Aprovados no DiscA
     dataFrameA = dataFrameA[dataFrameA['descricao'].str.contains('APROVADO')]
-
     series_aprovados = dataFrameA.discente.unique()
 
     df_finalB = pd.DataFrame()
@@ -59,10 +46,9 @@ def correlacao(request):
 
         df_finalB = pd.concat([df_finalB, linha])
 
-        # concatenando das tabelas
-        colunas = ['discente', 'media_final', 'nome']
-        dataFrameA = dataFrameA[colunas]
-        df_finalB = df_finalB[colunas]
+    colunas = ['discente', 'media_final', 'nome']
+    dataFrameA = dataFrameA[colunas]
+    df_finalB = df_finalB[colunas]
 
     conc = pd.concat([dataFrameA, df_finalB])
 
@@ -70,4 +56,30 @@ def correlacao(request):
     df = df.dropna()
     df_correlacao = df.corr()
 
-    return JsonResponse({'results': df_correlacao[discA][discB] })
+    # return JsonResponse({'results': df_correlacao[discA][discB] })
+    return df_correlacao[discA][discB]
+
+
+# Calcula a correlação de uma lista de disciplinas
+@csrf_protect
+def correlacao(request):
+    args = request.GET.get('lista')
+    lista_disciplinas = args.split(',')
+
+    # matriz de zeros
+    w, h = len(lista_disciplinas), len(lista_disciplinas)
+    content = [[0] * w for i in range(h)]
+    correlacoes = np.array(content, dtype='f')
+
+    # calculo das relacões sem repetição
+    for i in range(0, len(lista_disciplinas)):
+        for j in range(0, len(lista_disciplinas)):
+            if i == j:
+                correlacoes[i][j] = 1
+            if i < j:
+                correlacoes[i][j] = simpleCorrelacao(lista_disciplinas[i], lista_disciplinas[j])
+
+    df_retorno = pd.DataFrame(correlacoes, columns=lista_disciplinas)
+    df_retorno = df_retorno.set_axis(lista_disciplinas, axis=0, inplace=False)
+
+    return JsonResponse({'results':dataFrameToJson(df_retorno)})
