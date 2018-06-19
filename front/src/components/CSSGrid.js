3458275d2c80037aeb8f23f0695199713357849f
AndressaStefany <astefanysoliveira@gmail.com>
2018-06-19 10:21:08 -0300

    Layout front

--- front/src/components/CSSGrid.js
+++ front/src/components/CSSGrid.js
@@ -1,71 +1,76 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React,{Component} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import OutlinedButtons from './OutlinedButtons';
 
-const styles = theme => ({
-    container: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(12, 1fr)',
-        gridGap: `${theme.spacing.unit * 3}px`,
-    },
-    paper: {
-        padding: theme.spacing.unit,
+const styles = {
+    paper64: {
+        padding: '8px',
         textAlign: 'center',
-        color: theme.palette.text.secondary,
         whiteSpace: 'nowrap',
-        marginBottom: theme.spacing.unit,
-    },
-    divider: {
-        margin: `${theme.spacing.unit * 2}px 0`,
-    },
-});
-
-function CSSGrid(props) {
-    const { classes } = props;
-
-    return (
-        <div>
-            <Typography variant="subheading" gutterBottom>
-                <h1>Bacharelado em Ciências e Tecnologia</h1>
-                <br/>
-            </Typography>
-            <Grid container spacing={24}>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>
-                        <p className="App-center">1º período</p>
-                        <OutlinedButtons/>
-                    </Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>
-                        <p className="App-center">2º período</p>
-                        <OutlinedButtons/>
-                    </Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>
-                        <p className="App-center">3º período</p>
-                        <OutlinedButtons/>
-                    </Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>
-                        <p className="App-center">4º período</p>
-                        <OutlinedButtons/>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </div>
-    );
-}
-
-CSSGrid.propTypes = {
-    classes: PropTypes.object.isRequired,
+        marginBottom: '8px',
+        color: 'rgba(0, 0, 0, 0.54)',
+    }
 };
 
-export default withStyles(styles)(CSSGrid);
+class CSSGrid extends Component{
+    constructor(){
+        super();
+        this.state = {
+            periodo1: [],
+            periodo2: [],
+            periodo3: [],
+            periodo4: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://127.0.0.1:8000/api/disciplinas/?periodo=1')
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            this.setState({dimensao: data['dimensions']});
+            console.log(this.state.dimensao);
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <Typography variant="subheading" gutterBottom>
+                    <h1>Bacharelado em Ciências e Tecnologia</h1>
+                    <br/>
+                </Typography>
+                <Grid container spacing={24}>
+                    <Grid item xs={3}>
+                        <Paper style={styles.paper64}>
+                            <p className="App-center">1º período</p>
+                            <OutlinedButtons name="Teste1"/>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper style={styles.paper64}>
+                            <p className="App-center">2º período</p>
+                            <OutlinedButtons name="Teste2"/>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper style={styles.paper64}>
+                            <p className="App-center">3º período</p>
+                            <OutlinedButtons name="Teste3"/>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper style={styles.paper64}>
+                            <p className="App-center">4º período</p>
+                            <OutlinedButtons name="Teste4   "/>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default CSSGrid;
--- front/src/components/OutlinedButtons.js
+++ front/src/components/OutlinedButtons.js
@@ -1,38 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React,{Component} from 'react';
 import Button from '@material-ui/core/Button';
 
-const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit,
-    },
-    input: {
-        display: 'none',
-    },
-});
-
-function OutlinedButtons(props) {
-    const { classes } = props;
-    return (
-        <div>
-            <Button variant="outlined" color="secondary" className={classes.button}>
-                Pré-calculo
-            </Button>
-            <br/>
-            <Button variant="outlined" color="secondary" className={classes.button}>
-                Cálculo I
-            </Button>
-            <br/>
-            <Button variant="outlined" color="secondary" className={classes.button}>
-                Química Geral
-            </Button>
-        </div>
-    );
+class OutlinedButtons extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {title:props.name};
+    }
+    render(){
+        return (
+            <div>
+                <Button variant="outlined" color="secondary">
+                    {this.state.title}
+                </Button>
+            </div>
+        );
+    }
 }
 
-OutlinedButtons.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(OutlinedButtons);
+export default OutlinedButtons;
