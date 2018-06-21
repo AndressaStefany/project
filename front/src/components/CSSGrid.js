9120ac5e4f2fd593c57e8282d63ad639bf87cb23
AndressaStefany <astefanysoliveira@gmail.com>
2018-06-20 21:46:19 -0300

    Aparecendo os botões com o nome das disciplinas

--- front/src/components/CSSGrid.js
+++ front/src/components/CSSGrid.js
@@ -30,9 +30,50 @@ class CSSGrid extends Component{
             .then(results => {
                 return results.json();
             }).then(data => {
-            this.setState({dimensao: data['dimensions']});
-            console.log(this.state.dimensao);
-        })
+            data.results.map((array) => {
+                var newArray = this.state.periodo1;
+                newArray.push([array['codigo'], array['nome'], array['pre_requisito']]);
+                this.setState({
+                    periodo1: newArray
+                });
+            });
+        });
+        fetch('http://127.0.0.1:8000/api/disciplinas/?periodo=2')
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            data.results.map((array) => {
+                var newArray = this.state.periodo2;
+                newArray.push([array['codigo'], array['nome'], array['pre_requisito']]);
+                this.setState({
+                    periodo2: newArray
+                });
+            });
+        });
+        fetch('http://127.0.0.1:8000/api/disciplinas/?periodo=3')
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            data.results.map((array) => {
+                var newArray = this.state.periodo3;
+                newArray.push([array['codigo'], array['nome'], array['pre_requisito']]);
+                this.setState({
+                    periodo3: newArray
+                });
+            });
+        });
+        fetch('http://127.0.0.1:8000/api/disciplinas/?periodo=4')
+            .then(results => {
+                return results.json();
+            }).then(data => {
+            data.results.map((array) => {
+                var newArray = this.state.periodo4;
+                newArray.push([array['codigo'], array['nome'], array['pre_requisito']]);
+                this.setState({
+                    periodo4: newArray
+                });
+            });
+        });
     }
 
     render(){
@@ -46,25 +87,33 @@ class CSSGrid extends Component{
                     <Grid item xs={3}>
                         <Paper style={styles.paper64}>
                             <p className="App-center">1º período</p>
-                            <OutlinedButtons name="Teste1"/>
+                            {this.state.periodo1.map((linha)=>
+                                <OutlinedButtons id={linha[0]} name={linha[1]} preid={linha[3]}/>
+                            )}
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper style={styles.paper64}>
                             <p className="App-center">2º período</p>
-                            <OutlinedButtons name="Teste2"/>
+                            {this.state.periodo2.map((linha)=>
+                                <OutlinedButtons id={linha[0]} name={linha[1]} preid={linha[3]}/>
+                            )}
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper style={styles.paper64}>
                             <p className="App-center">3º período</p>
-                            <OutlinedButtons name="Teste3"/>
+                            {this.state.periodo3.map((linha)=>
+                                <OutlinedButtons id={linha[0]} name={linha[1]} preid={linha[3]}/>
+                            )}
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper style={styles.paper64}>
                             <p className="App-center">4º período</p>
-                            <OutlinedButtons name="Teste4   "/>
+                            {this.state.periodo4.map((linha)=>
+                                <OutlinedButtons id={linha[0]} name={linha[1]} preid={linha[3]}/>
+                            )}
                         </Paper>
                     </Grid>
                 </Grid>
--- front/src/components/OutlinedButtons.js
+++ front/src/components/OutlinedButtons.js
@@ -1,15 +1,25 @@
 import React,{Component} from 'react';
 import Button from '@material-ui/core/Button';
 
+const styles = {
+    padding : {
+        marginBottom: '4%'
+    }
+};
+
 class OutlinedButtons extends Component {
     constructor(props) {
         super(props);
-        this.state = {title:props.name};
+        this.state = {
+            title:props.name,
+            id:props.id,
+            preId:props.preid
+        };
     }
     render(){
         return (
             <div>
-                <Button variant="outlined" color="secondary">
+                <Button variant="outlined" color="secondary" style={styles.padding}>
                     {this.state.title}
                 </Button>
             </div>
