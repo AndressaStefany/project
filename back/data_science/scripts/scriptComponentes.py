deec94b93edceada1bea6489a07ed53a368ca3fe
AndressaStefany <astefanysoliveira@gmail.com>
2018-07-30 17:31:31 -0300

    #4 correção do problema das materias repetidas

--- back/data_science/scripts/scriptComponentes.py
+++ back/data_science/scripts/scriptComponentes.py
@@ -1,12 +1,25 @@
 import pandas as pd
 
 df_curriculo = pd.read_csv('../curriculo-componente-graduacao.csv', sep=';')
+df_curriculo = df_curriculo[df_curriculo['semestre_oferta'] != 0]
+
+series_idcomponentecurricular = df_curriculo['id_componente_curricular'].unique()
+
+df_curriculoSelec = pd.DataFrame()
+
+for l in series_idcomponentecurricular:
+    linha = df_curriculo[ df_curriculo['id_componente_curricular'] == l]
+    linhadf = df_curriculo[df_curriculo['id_curriculo_componente'] == linha.id_curriculo_componente.max()]
+    
+    df_curriculoSelec = pd.concat([df_curriculoSelec, linhadf])
+    
 colunas_2 = ['id_componente_curricular', 'semestre_oferta', 'tipo_vinculo_componente']
-df_curriculo = df_curriculo[colunas_2]
+df_curriculoSelec = df_curriculoSelec[colunas_2].drop_duplicates()
 
 df_materias = pd.read_csv('../componentes-curriculares-presenciais-ok.csv')
+df_materias = df_materias.drop_duplicates()
 
-df_materiasSemestre = pd.merge(df_materias, df_curriculo, left_on='id_componente', right_on='id_componente_curricular')
+df_materiasSemestre = pd.merge(df_materias, df_curriculoSelec, left_on='id_componente', right_on='id_componente_curricular')
 
 unidade = 'ESCOLA DE CIÊNCIAS E TECNOLOGIA'
 vinculo_componente = 'OBRIGATÓRIO'
