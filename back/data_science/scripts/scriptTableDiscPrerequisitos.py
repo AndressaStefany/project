cc82f30249398c645ad8dcf6e9419128a86b6c1d
AndressaStefany <astefanysoliveira@gmail.com>
2018-07-30 18:26:26 -0300

    #4 Table de disciplinas e pre requisitos com os nomes

--- back/data_science/scripts/scriptTableDiscPrerequisitos.py
+++ back/data_science/scripts/scriptTableDiscPrerequisitos.py
@@ -7,6 +7,7 @@ coluna_periodo = df_disciplinas['semestre_oferta']
 df_newTable = df_disciplinas[['codigo', 'nome']]
 df_newTable['periodo'] = coluna_periodo
 df_newTable['pre_requisito'] = '-'
+df_newTable['nome_prereq'] = '-'
 
 for i in df_newTable['codigo']:# disciplinas
     # frase prerequisito
@@ -14,6 +15,8 @@ for i in df_newTable['codigo']:# disciplinas
 
     for j in range(0, len(found)):# linha do df - erro aqui
         if found[j]:
+            nome = df_newTable[df_newTable['codigo'] == i]['nome'].values[0]
             df_newTable['pre_requisito'][j] = (df_newTable['pre_requisito'][j]+','+i).replace('-,','')
+            df_newTable['nome_prereq'][j] = (df_newTable['nome_prereq'][j]+','+nome).replace('-,','')
 
-df_newTable.to_csv('../disciplinas_prerequisitos.csv')
+df_newTable.to_csv('../disciplinas_prerequisitosnome.csv')
