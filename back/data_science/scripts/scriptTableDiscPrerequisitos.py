9dc4f0b140d4d49e9b93f650d5e9eb5852b858d5
AndressaStefany <astefanysoliveira@gmail.com>
2018-07-30 18:00:20 -0300

    #4 Table de disciplinas e pre requisitos

---
+++ back/data_science/scripts/scriptTableDiscPrerequisitos.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df_disciplinas = pd.read_csv('../componentes-semestres.csv')
+df_disciplinas = df_disciplinas.fillna('-')
+
+coluna_periodo = df_disciplinas['semestre_oferta']
+df_newTable = df_disciplinas[['codigo', 'nome']]
+df_newTable['periodo'] = coluna_periodo
+df_newTable['pre_requisito'] = '-'
+
+for i in df_newTable['codigo']:# disciplinas
+    # frase prerequisito
+    found = df_disciplinas['pre_requisito'].apply(lambda x: x.count(i))
+
+    for j in range(0, len(found)):# linha do df - erro aqui
+        if found[j]:
+            df_newTable['pre_requisito'][j] = (df_newTable['pre_requisito'][j]+','+i).replace('-,','')
+
+df_newTable.to_csv('../disciplinas_prerequisitos.csv')
